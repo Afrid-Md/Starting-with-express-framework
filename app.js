3d4b4af62d01bbcbc0e87f3e0beb3a764b53300e
Afrid.md <122868488+Afrid-Md@users.noreply.github.com>
2023-09-19 15:16:06 +0530

    added '/add-product' route and added form(task-2)

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next)=>{
-    console.log('In the middleware');
-    next();
-})
+app.use(bodyParser.urlencoded({extended:false}));
 
-app.use((req, res, next)=>{
-    console.log('In the another middleware');
-    res.send({key:"value"});
-})
+app.use('/add-product',(req, res, next)=>{
+    res.send('<form action="/product" method="POST"><label>Name of the product</label><input type="text" name="title"/><label>Quantity</label><input type="number" name="quantity"/><button type="submit">Add</button></form>');
+});
+
+app.post('/product',(req, res, next)=>{
+    console.log(req.body.title);
+    console.log(req.body.quantity);
+    res.redirect('/');
+});
+
+app.use('/',(req, res, next)=>{
+    res.send('<h1>This is home page</h1>');
+});
 
 
 app.listen(4000);
