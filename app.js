a273bf715c3d95e03d16fadf97f2edb44f4a81e9
Afrid.md <122868488+Afrid-Md@users.noreply.github.com>
2023-09-28 18:57:54 +0530

    Serving HTML , CSS and JS (VIEWS)(Task-5)

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
+const path = require('path');
+
 const express = require('express');
+
 const bodyParser = require('body-parser');
 
 const app = express();
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require('./routes/shop');
+const contactusRoutes = require('./routes/contact-us');
+const successRoutes = require('./routes/success');
+
 
 app.use(bodyParser.urlencoded({extended:false}));
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
-
+app.use(contactusRoutes);
+app.use(successRoutes);
 app.use((req,res,next)=>{
-    res.status(404).send('<h1>Page not found</h1>')
-})
+    res.status(404).sendFile(path.join(__dirname, 'views', 'error.html'));
+});
 
 app.listen(4000);
